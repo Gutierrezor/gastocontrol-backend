bd2027a5a19517b97186305b7cb247cbfb363185
julian andres gutierrez ordoñez <juliangutierrezor@gmail.com>
2025-05-27 18:52:49 -0500

    union

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
 import { Sequelize } from 'sequelize';
 
-// Usa la base de datos definida en .env o por defecto 'gastocontro'
-const dbName = process.env.DB_NAME || 'gastocontro';
-const dbUser = process.env.DB_USER || 'postgres';
-const dbPass = process.env.DB_PASS || '123456789';
-const dbHost = process.env.DB_HOST || 'localhost';
-const dbPort = process.env.DB_PORT || 5432;
+const connectionString = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPass, {
-  host: dbHost,
-  port: dbPort,
+const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
-  logging: false, 
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
 });
 
 export default sequelize;
