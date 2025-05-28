7de2f0f190a401e9c316a04c13b38dcd491430aa
julian andres gutierrez ordoñez <juliangutierrezor@gmail.com>
2025-05-28 15:00:05 -0500

    excel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import expensesExportRouter from '../routes/expensesExport.js';
 
 const app = express();
+
+// Habilita CORS para tu frontend
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 app.use('/api/expenses', expensesExportRouter);
 
